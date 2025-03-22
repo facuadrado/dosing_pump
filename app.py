7f4a626c89283d3f3f5262704b88fc48fe0ecfe7
facuadrado <facrodz@gmail.com>
2025-03-21 21:07:14 -0400

    Added endpoint to get scheduled jobs. Modified endopints to POST.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException, Response, BackgroundTasks
 from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi.responses import JSONResponse, PlainTextResponse
 import RPi.GPIO as GPIO
 import time
 import os
 import csv
 from datetime import datetime
+import csv
+import json
 
 #================================ CONSTANTS ================================#
 PIN_1 = "pin_1"
@@ -36,8 +39,10 @@ PUMP_CALIBRATION_ML_PER_SECOND = {
     2: RIGHT_FLOW_RATE_ML_PER_SECOND
 }
 
-LEFT_DAILY_SCHEDULED_DOSE = 6
-RIGHT_DAILY_SCHEDULED_DOSE = 6
+LEFT_DAILY_SCHEDULED_DOSE = 10
+LEFT_DOSES_PER_DAY = 2
+
+RIGHT_DAILY_SCHEDULED_DOSE = 10
 
 # Initializing GPIO
 GPIO.setmode(GPIO.BCM)
@@ -67,7 +72,6 @@ def record_data_csv(doser_id: int, mode: str, ml: float):
         writer.writerow([date_string, doser_id, time_string, ml, mode])
 
 def dose(doser_id: int, mode: str, ml: float):
-
     seconds = ml / PUMP_CALIBRATION_ML_PER_SECOND[doser_id]
     pin_1 = DOSER_GPIO_MAP[doser_id][PIN_1]
     pin_2 = DOSER_GPIO_MAP[doser_id][PIN_2]
@@ -81,7 +85,7 @@ def dose(doser_id: int, mode: str, ml: float):
     record_data_csv(doser_id, mode, ml)
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(record_data_csv, args=[1, "Scheduled", LEFT_DAILY_SCHEDULED_DOSE/2], trigger='interval', hours=12, id='scheduled_dose')
+scheduler.add_job(dose, args=[2, "Scheduled", LEFT_DAILY_SCHEDULED_DOSE/LEFT_DOSES_PER_DAY], trigger='interval', hours=24/LEFT_DOSES_PER_DAY, id='scheduled_dose')
 scheduler.start()
 
 #================================ Fast API ================================#
@@ -89,7 +93,7 @@ scheduler.start()
 app = FastAPI()
 
 # Endpoint to manually dose for x mL
-@app.get("/dose/{doser_id}")
+@app.post("/dose/{doser_id}")
 async def send_dose_task(doser_id: int, ml: float, background_tasks: BackgroundTasks):
     """
     Manually start the dosing process for a specific amount of mL.
@@ -110,10 +114,10 @@ async def send_dose_task(doser_id: int, ml: float, background_tasks: BackgroundT
                             detail=f"Invalid dosing amount. Must be greater than 0mL and smaller/equal to {UPPER_LIMIT}mL."
                             )
     background_tasks.add_task(dose, doser_id, "Manual", ml)
-    return {"message": f"Sent dose command for {ml}mL on doser {doser_id}."}
+    return PlainTextResponse(content=f"Sent dose command for {ml}mL on doser {doser_id}.", status_code=200)
 
 # Endpoint to prime the doser for 5mL
-@app.get("/prime/{doser_id}")
+@app.post("/prime/{doser_id}")
 async def send_prime_task(doser_id: int, background_tasks: BackgroundTasks):
     """
     Prime the pump to distribute 5mL to get the liquid flowing.
@@ -129,28 +133,65 @@ async def send_prime_task(doser_id: int, background_tasks: BackgroundTasks):
     
     ml = PRIMER_ML
     background_tasks.add_task(dose, doser_id, "Priming", ml)
-    return {"message": f"Sent command to prime {ml}mL on doser {doser_id}."}
+    return PlainTextResponse(content=f"Sent command to prime {ml}mL on doser {doser_id}.", status_code=200)
 
-# Endpoint to get dosing report
 @app.get("/dosing-report")
 def get_dosing_report():
     """
-    Get CSV dosing report
-    
+    Get CSV dosing report as JSON
+
     Returns:
-    - A plain text with the dosing report data.
+    - A JSON representation of the dosing report data.
     """
     # Check if file exists
     file_exists = os.path.isfile(csv_file)
     if not file_exists:
         raise HTTPException(status_code=404, detail="CSV file not found!")
 
-    # Read CSV file
-    with open(csv_file, "r") as file:
-        csv_content = file.read()
-    
-    # Return the CSV content as plain text
-    return Response(status_code=200, content=csv_content, media_type="text/csv")
+    # Read and parse CSV file
+    try:
+        with open(csv_file, "r") as file:
+            reader = csv.DictReader(file)
+            csv_data = [row for row in reader]  # Convert CSV rows to list of dictionaries
+
+        # Return the CSV content as JSON
+        return JSONResponse(
+            content=csv_data,
+            status_code=200
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing CSV file: {str(e)}")
+
+@app.get("/jobs")
+def list_obs():
+    """
+    Get Scheduled report
+
+    Returns:
+    - A JSON object showing next dose for each schedule.
+    """
+    try:
+        # Fetch jobs from scheduler
+        jobs = scheduler.get_jobs()
+        job_data = [
+            {
+                "id": job.id,
+                "next_run": job.next_run_time.strftime("%Y-%m-%d %H:%M:%S")
+                if job.next_run_time else None
+            }
+            for job in jobs
+        ]
+
+        # Return as JSON response
+        return JSONResponse(
+            content=job_data,
+            status_code=200
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"An error occurred while retrieving jobs: {str(e)}"
+        )
 
 # Shutdown scheduler on application shutdown
 @app.on_event("shutdown")
